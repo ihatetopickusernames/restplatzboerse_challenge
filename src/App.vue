5a43ccee463d90a67dbd1b36f1ded99884a4b3f0
barburak <barbara92@kaini.at>
2022-07-08 20:09:03 +0200

    design adaptions, layouting, accessibility, ellipis when text is overflowing

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
-  <!--  <h1>Restplatzboerse Challenge</h1>-->
-  <!--  <h2>Subtitel</h2>-->
-  <!--  <h3>Tier</h3>-->
-  <!--  <div class="price"><span class="price-label">ab</span> € 245</div>-->
-  <TeaserSlider> </TeaserSlider>
+  <main class="container mt-1 mb-1">
+    <TeaserSlider> </TeaserSlider>
+  </main>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import TeaserSlider from "./components/TeaserSlider.vue";
+
 require("@/assets/main.scss");
 
 @Options({
--- src/components/TeaserSlider.vue
+++ src/components/TeaserSlider.vue
@@ -1,12 +1,34 @@
 <template>
-  <h1>Zootiere</h1>
-  <h2>Zufällige Auswahl an Zootieren</h2>
-  <a href="https://www.restplatzboerse.at/" title="Zur Hauptseite">Alle</a>
-  <div v-for="animal in animals" :key="animal.id" class="images">
-    <img :src="animal.image_link" alt="{{animal.name}}" />
-    <h3>{{ animal.name }}</h3>
-    <div class="weight">
-      <span class="weight-label">bis zu</span> {{ animal.weight_max }} kg
+  <div class="is-flex is-justify-content-space-between">
+    <div>
+      <h1>Zootiere</h1>
+      <h2 class="ellipsis">Zufällige Auswahl an Zootieren</h2>
+    </div>
+    <a
+      href="https://www.restplatzboerse.at/"
+      title="Zur Restplatzbörse Startseite."
+      >Alle</a
+    >
+  </div>
+  <div class="slider-content grid-display">
+    <div class="card" v-for="animal in animals" :key="animal.id">
+      <div class="card-image">
+        <figure class="image is-5by3">
+          <img :src="animal.image_link" :alt="animal.name" />
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media-content">
+          <div class="is-flex is-justify-content-space-between">
+            <!-- added accessibility with tabindex property. users can now access content by pressing tab key. -->
+            <h3 class="ellipsis" tabindex="0">{{ animal.name }}</h3>
+            <div class="weight has-text-right ellipsis">
+              <span class="weight-label">bis zu</span>
+              {{ animal.weight_max }} kg
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
