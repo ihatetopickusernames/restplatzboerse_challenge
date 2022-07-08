ae90059f3722171aa1810871a3b0f8ae456282eb
barburak <barbara92@kaini.at>
2022-07-08 13:49:45 +0200

    project setup

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Restplatzboerse Challenge</h1>
+  <h2>Subtitel</h2>
+  <h3>Tier</h3>
+  <div class="price"><span class="price-label">ab</span> € 245</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "./components/HelloWorld.vue";
+require("@/assets/main.scss");
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
+</script>
